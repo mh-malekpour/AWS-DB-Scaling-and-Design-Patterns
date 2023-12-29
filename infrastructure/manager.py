8024c4b01360a401d0f2205b4cf2ab4f18ec753d
Mohammadhossein Malekpour <mohammadhossein.malekpour@gmail.com>
2023-12-29 12:39:16 -0500

    update manger node commands

--- infrastructure/manager.py
+++ infrastructure/manager.py
@@ -16,20 +16,57 @@ for instance in instance_details:
         instance_ids.append(instance['InstanceID'])
 
 commands = [
-    # Install docker
-    'sudo apt-get update',
-    'sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common',
-    'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -',
-    'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"',
-    'sudo apt-get update',
-    'apt-cache policy docker-ce',
-    'sudo apt-get install -y docker-ce',
-    'sudo usermod -aG docker ubuntu',
-    'sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
-    'sudo chmod +x /usr/local/bin/docker-compose',
-    # Deploy worker
-    'cd /home/ubuntu && git clone https://github.com/mh-malekpour/Deploying-ML-Model-on-AWS.git',
-    'cd /home/ubuntu/Deploying-ML-Model-on-AWS/orchestrator && sudo docker-compose up --build'
+    # phase 1
+    "sudo mkdir -p /opt/mysqlcluster/home",
+    "cd /opt/mysqlcluster/home && sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz",
+    "cd /opt/mysqlcluster/home && sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz",
+    "cd /opt/mysqlcluster/home && sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc",
+    "echo 'export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' | sudo tee /etc/profile.d/mysqlc.sh && echo 'export PATH=$MYSQLC_HOME/bin:$PATH' | sudo tee -a /etc/profile.d/mysqlc.sh && source /etc/profile.d/mysqlc.sh",
+    "sudo apt-get update && sudo apt-get -y install libncurses5",
+    # phase2
+    "sudo mkdir -p /opt/mysqlcluster/deploy",
+    "sudo mkdir conf",
+    "sudo mkdir mysqld_data",
+    "sudo mkdir ndb_data",
+    """
+    cd conf && sudo cat <<EOF > my.cnf
+    [mysqld]
+    ndbcluster
+    datadir=/opt/mysqlcluster/deploy/mysqld_data
+    basedir=/opt/mysqlcluster/home/mysqlc
+    port=3306
+    EOF
+    """,
+    """
+    cd conf && sudo cat <<EOF > config.ini
+    [ndb_mgmd]
+    hostname=ip-10-0-1-225.ec2.internal
+    datadir=/opt/mysqlcluster/deploy/ndb_data
+    nodeid=1
+    
+    [ndbd default]
+    noofreplicas=3
+    datadir=/opt/mysqlcluster/deploy/ndb_data
+    
+    [ndbd]
+    hostname=ip-10-0-1-254.ec2.internal
+    nodeid=2
+    
+    [ndbd]
+    hostname=ip-10-0-1-126.ec2.internal
+    nodeid=3
+    
+    [ndbd]
+    hostname=ip-10-0-1-6.ec2.internal
+    nodeid=4
+    
+    [mysqld]
+    nodeid=50
+    """,
+    # phase 3
+    "cd /opt/mysqlcluster/home/mysqlc && sudo scripts/mysql_install_db --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data",
+    "sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/",
+    'mysqld --defaults-file="/opt/mysqlcluster/deploy/conf/my.cnf" --user=root &'
 ]
 
 def send_command(instance_ids, command):
@@ -65,9 +102,6 @@ for command in commands:
         break  # Stop executing further commands if one has already failed
 
     command_id = send_command(instance_ids, command)
-    if 'docker-compose up' in command:
-        print("running docker-compose up on instances")
-        break
 
     for instance_id in instance_ids:
         status = check_command_status(command_id, instance_id)
